94c094fc5a4b153992902c546a0171cb4486f0a4
Liam Gleason <lgleason@pitco.com>
2022-07-25 17:05:31 -0400

    Cleaning up Rust

--- rustmaz/src/main.rs
+++ rustmaz/src/main.rs
@@ -26,11 +26,11 @@ impl Wall {
 #[derive(Clone, PartialEq)]
 struct Cell {
     walls: HashSet<Wall>,
-    coords: (u32, u32),
+    coords: (usize, usize),
 }
 
 impl Cell {
-    fn new(coords: (u32, u32)) -> Self {
+    fn new(coords: (usize, usize)) -> Self {
         let walls = [Wall::Top, Wall::Bottom, Wall::Left, Wall::Right];
         Cell { walls: HashSet::from(walls), coords }
     }
@@ -68,10 +68,10 @@ impl Cell {
 
 struct Maz {
     value: Vec<u8>,
-    size: u32,
-    in_maze_list: Vec<(u32, u32)>,
+    size: usize,
+    in_maze_list: Vec<(usize, usize)>,
     cells: Vec<Cell>,
-    to_add: Vec<(u32, u32)>
+    to_add: Vec<(usize, usize)>
 
 }
 
@@ -90,13 +90,13 @@ impl std::ops::Deref for Maz {
 }
 
 impl Maz {
-    fn new(size: u32) -> Self {
-        let mut i: u32 = 0;
-        let total_size: u32 = size * size;
+    fn new(size: usize) -> Self {
+        let mut i: usize = 0;
+        let total_size: usize = size * size;
         let mut val_holder: Vec<u8> = vec![];
         let mut cell_holder: Vec<Cell> = vec![];
-        let mut curr_x: u32 = 0;
-        let mut curr_y: u32 = 0;
+        let mut curr_x: usize = 0;
+        let mut curr_y: usize = 0;
         while i < total_size {
             val_holder.push(0);
             cell_holder.push(Cell::new((curr_x, curr_y)));
@@ -110,61 +110,9 @@ impl Maz {
         }
         Maz { value: val_holder, size, in_maze_list: vec![], cells: cell_holder, to_add: vec![]}
     }
-    fn get_at(&self, x: u32, y: u32) -> u8 {
-        self.value[((&self.size * x) + y) as usize]
-    }
-    fn get_cell_at(&self, x: u32, y: u32) -> usize {
-        for cell in 0..self.cells.len() {
-            if self.cells[cell].coords == (x, y) {
-                return cell
-            }
-        }
-        0
-    }
-    fn set_at(&mut self, x: u32, y: u32, value: u8) {
-        self.value[((&self.size * x) + y) as usize] = value;
-    }
-    fn open_border(&mut self, a: (u32, u32), b: (u32, u32)) {
-        let y1 = a.0 as isize;
-        let y2 = b.0 as isize;
-        let x1 = a.1 as isize;
-        let x2 = b.1 as isize;
-
-        let val1 = self.get_cell_at(a.0, a.1) as usize;
-        let sides1 = self.cells[self.get_cell_at(a.0, a.1)].to_u8();
-        let val2 = self.get_cell_at(b.0, b.1) as usize;
-        let sides2 = self.cells[self.get_cell_at(b.0, b.1)].to_u8();
-
-        if x2 - x1 == 1 {
-            self.value[val1] = to_encoded(false, sides1, Wall::Left);
-            self.value[val2] = to_encoded(false, sides2, Wall::Right);
-            //self.get_cell_at(a.0, a.1).set_wall(false, Wall::Left);
-            //self.get_cell_at(b.0, b.1).set_wall(false, Wall::Right);
-        } else if x1  - x2  == 1 {
-            self.value[val1] = to_encoded(false, sides1, Wall::Right);
-            self.value[val2] = to_encoded(false, sides2, Wall::Left);
-            //self.get_cell_at(a.0, a.1).set_wall(false, Wall::Right);
-            //self.get_cell_at(b.0, b.1).set_wall(false, Wall::Left);
-        } else if y2  - y1  == 1 {
-            self.value[val1] = to_encoded(false, sides1, Wall::Top);
-            self.value[val2] = to_encoded(false, sides2, Wall::Bottom);
-            //self.get_cell_at(a.0, a.1).set_wall(false, Wall::Top);
-            //self.get_cell_at(b.0, b.1).set_wall(false, Wall::Bottom);
-        } else if y1  - y2  == 1 {
-            self.value[val1] = to_encoded(false, sides1, Wall::Bottom);
-            self.value[val2] = to_encoded(false, sides2, Wall::Top);
-            //self.get_cell_at(a.0, a.1).set_wall(false, Wall::Bottom);
-            //self.get_cell_at(b.0, b.1).set_wall(false, Wall::Top);
-        }
-    }
     fn generate(&mut self) {
-        self.carve_from((0, 0));
-        /*for i in 1..self.value.len() {
-            self.value[i] = self.cells[self.get_cell_at(self.in_maze_list[i].0, self.in_maze_list[i].1)].to_u8();
-        }*/
-    }
-    fn carve_from(&mut self, mut coords: (u32, u32)) {
-        let mut prevlist: Vec<(u32, u32)> = vec![];
+        let mut coords= (0, 0);
+        let mut prevlist: Vec<(usize, usize)> = vec![];
         loop {
             if self.to_add.contains(&coords) {
                 self.to_add.remove(self.to_add.len() - 1);
@@ -245,17 +193,48 @@ impl Maz {
                 self.in_maze_list.push(next_coords);
                 self.open_border(coords, next_coords);
             }
-
-            /*match next_side {
-            Wall::Top => { self.carve_from((coords.0, coords.1 - 1)) },
-            Wall::Bottom => { self.carve_from((coords.0, coords.1 + 1)) },
-            Wall::Left => { self.carve_from((coords.0 - 1, coords.1)) },
-            Wall::Right => {  self.carve_from((coords.0 + 1, coords.1)) },
-            _ => panic!("TOO BIG BRO!")
-        };*/
             if self.to_add.len() <= 0 && self.in_maze_list.len() == (self.size * self.size) as usize {
                 break;
             }
+        }
+    }
+    fn get_value_at(&self, x: usize, y: usize) -> u8 {
+        self.value[((&self.size * x) + y) as usize]
+    }
+    fn set_value_at(&mut self, x: usize, y: usize, value: u8) {
+        self.value[((&self.size * x) + y) as usize] = value;
+    }
+    fn get_index_of_cell_at(&self, x: usize, y: usize) -> usize {
+        for cell in 0..self.cells.len() {
+            if self.cells[cell].coords == (x, y) {
+                return cell
+            }
+        }
+        0
+    }
+    fn open_border(&mut self, a: (usize, usize), b: (usize, usize)) {
+        let y1 = a.0 as isize;
+        let y2 = b.0 as isize;
+        let x1 = a.1 as isize;
+        let x2 = b.1 as isize;
+
+        let val1 = self.get_index_of_cell_at(a.0, a.1) as usize;
+        let sides1 = self.cells[self.get_index_of_cell_at(a.0, a.1)].to_u8();
+        let val2 = self.get_index_of_cell_at(b.0, b.1) as usize;
+        let sides2 = self.cells[self.get_index_of_cell_at(b.0, b.1)].to_u8();
+
+        if x2 - x1 == 1 {
+            self.value[val1] = to_encoded(false, sides1, Wall::Left);
+            self.value[val2] = to_encoded(false, sides2, Wall::Right);
+        } else if x1  - x2  == 1 {
+            self.value[val1] = to_encoded(false, sides1, Wall::Right);
+            self.value[val2] = to_encoded(false, sides2, Wall::Left);
+        } else if y2  - y1  == 1 {
+            self.value[val1] = to_encoded(false, sides1, Wall::Top);
+            self.value[val2] = to_encoded(false, sides2, Wall::Bottom);
+        } else if y1  - y2  == 1 {
+            self.value[val1] = to_encoded(false, sides1, Wall::Bottom);
+            self.value[val2] = to_encoded(false, sides2, Wall::Top);
         }
     }
 }
@@ -271,7 +250,7 @@ fn to_encoded(value: bool, initial: u8, side: Wall) -> u8 {
 }
 
 fn main() {
-    //let size: u32 = rand::thread_rng().gen_range(2..41);
+    //let size: usize = rand::thread_rng().gen_range(2..41);
     let size= 30;
     let mut bob = Maz::new(size);
     bob.generate();
